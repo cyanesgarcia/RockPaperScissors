faaa8122cb86e93273fe44ff2684e84c26eae42e
cyanesgarcia <cyanesgarcia@my.apsu.edu>
2018-06-29 16:33:41 +0000

    RockPaperScissors java

--- app/src/main/java/com/yanes/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/yanes/rockpaperscissors/MainActivity.java
@@ -1,10 +1,13 @@
 package com.yanes.rockpaperscissors;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -17,10 +20,22 @@ public class MainActivity extends Activity {
     String myChoise, cpuChoise, result;
 
     Random r;
+    int game_money;
+    int total_money;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        game_money = intent.getIntExtra(RockPaperScissors.SGameMoney, 0);
+        total_money = intent.getIntExtra(RockPaperScissors.STotalMoney, 0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.totalmoney);
+        tv.setText("" + total_money);
+        tv = (TextView) findViewById(R.id.gamemoney);
+        tv.setText("" + game_money);
+
 
         iv_cpu=(ImageView) findViewById(R.id.iv_cpu);
         iv_me=(ImageView) findViewById(R.id.iv_me);
@@ -45,8 +60,8 @@ public class MainActivity extends Activity {
             public void onClick(View view) {
                 myChoise ="paper";
                 iv_me.setImageResource(R.drawable.paper);
-
                 calculate();
+                back();
             }
         });
         b_scissors.setOnClickListener(new View.OnClickListener() {
@@ -55,9 +70,34 @@ public class MainActivity extends Activity {
                 myChoise ="scissors";
                 iv_me.setImageResource(R.drawable.scissors);
                 calculate();
+                back();
             }
         });
+
     }
+    public void back(){
+        String str_total = Integer.toString(total_money);
+        str_total = str_total.trim();
+        RockPaperScissors.STotalMoney = str_total;
+
+        Intent intent1 = new Intent(this, MainActivity.class);
+        intent1.putExtra(RockPaperScissors.STotalMoney, str_total);
+        game_money = 0;
+        String game = Integer.toString(game_money);
+        RockPaperScissors.SGameMoney = game;
+        intent1.putExtra(RockPaperScissors.SGameMoney, game);
+
+
+        setResult(RESULT_OK, intent1);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                MainActivity.this.finish();
+            }
+        }, 3000);
+
+    }
+
     public void calculate(){
     int cpu= r.nextInt(3);
     if(cpu==0){
@@ -75,29 +115,42 @@ public class MainActivity extends Activity {
     }
     if(myChoise.equals("rock") && cpuChoise.equals("scissors")){
             result ="You win";
+        total_money += game_money * 2;
+
     }
     if(myChoise.equals("paper") && cpuChoise.equals("rock")){
             result ="You win";
+        total_money += game_money * 2;
+
     }
     if(myChoise.equals("paper") && cpuChoise.equals("scissors")){
             result ="You lose";
     }
     if(myChoise.equals("scissors") && cpuChoise.equals("paper")){
             result ="You win";
+        total_money += game_money * 2;
+
     }
     if(myChoise.equals("scissors") && cpuChoise.equals("rock")){
             result ="You lose";
     }
     if(myChoise.equals("scissors") && cpuChoise.equals("scissors")){
             result ="Draw";
+        total_money += game_money;
+
     }
     if(myChoise.equals("rock") && cpuChoise.equals("rock")){
             result ="Draw";
+        total_money += game_money;
+
     }
     if(myChoise.equals("paper") && cpuChoise.equals("paper")){
             result ="Draw";
+        total_money += game_money;
+
     }
 
         Toast.makeText(MainActivity.this, result,Toast.LENGTH_SHORT).show();
     }
+
 }
